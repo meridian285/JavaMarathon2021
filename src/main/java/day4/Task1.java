e21835b0384bc37d87e26ed7ea97ed5727841709
Alex <akelizarovav@gmail.com>
2022-11-24 21:14:09 +0300

    21312

--- src/main/java/day4/Task1.java
+++ src/main/java/day4/Task1.java
@@ -1,5 +1,7 @@
 package day4;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,14 +11,26 @@ public class Task1 {
         Scanner scanner = new Scanner(System.in);
         int sizeArray = scanner.nextInt();
         int[] array = new int[sizeArray];
-        for(int i:array){
+        int oddNumbers = 0;
+        int evenNumbers = 0;
+        int moreEight = 0;
+        int summ = 0;
+        for(int i=0;i<array.length;i++){
             array[i] = random.nextInt(10);
-            System.out.println(array[i]);
-        }
-        System.out.println("Длинна массива = "+array.length);
-        for (int i:array){
+            summ +=array[i];
             if(array[i]>8)
-                System.out.println(array[i]);
+                moreEight += 1;
+            if(array[i] % 2 == 0) {
+                evenNumbers += 1;
+            }else oddNumbers+=1;
+
         }
+        System.out.print(Arrays.toString(array));
+        System.out.println();
+        System.out.println("Длинна массива: "+array.length);
+        System.out.println("Количестве чисел больше 8: "+moreEight);
+        System.out.println("Количестве четных чисел: "+evenNumbers);
+        System.out.println("Количестве нечетных чисел: "+oddNumbers);
+        System.out.println("Сумме всех элементов массива: "+summ);
     }
 }
