a7a0f5dbd35f38a9e1d6169a7994840fb3be695b
Aleksandr Elizarov <akelizarovav@gmail.com>
2022-11-25 16:22:55 +0300

    23141234

---
+++ src/main/java/day6/Motorbike.java
@@ -0,0 +1,41 @@
+package day6;
+
+public class Motorbike {
+    private int age;
+    private String color;
+    private String model;
+
+    public void info(){
+        System.out.println("Это мотоцикл!");
+    }
+
+    public Motorbike(int age, String color, String model) {
+        this.age = age;
+        this.color = color;
+        this.model = model;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
